2fa6aab1296ba9bf335145395ff7cf81931d2292
thisisommore <ommore501@gmail.com>
2022-02-02 17:34:19 +0000

    generate_abi: fix cleaning

--- generate_abi.sh
+++ generate_abi.sh
@@ -4,4 +4,5 @@ svn checkout https://github.com/NetBlockGit/smart-contracts/trunk/contracts \
                     --username=$GITPOD_GIT_USER_NAME --password=$GITHUB_TOKEN
 solc --abi contracts/Blocklist.sol -o out
 abigen --pkg blocklist --out=../generated/smartcontract/blocklist/blocklist.go out/Blocklist.abi 
+cd -
 rm -rf temp
