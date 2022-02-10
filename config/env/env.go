d85b84e070f14ab1cd0a57b764c76d29a8dbb7ad
Om More <51229945+thisisommore@users.noreply.github.com>
2022-02-10 16:58:56 +0530

    Config env path from root (#22)
    * Config env file path from param
    
    * fix: .env file path in app.go

--- config/env/env.go
+++ config/env/env.go
@@ -3,27 +3,16 @@ package env
 import (
 	"log"
 	"os"
-	"path/filepath"
-	"runtime"
 
 	"github.com/joho/godotenv"
 )
 
-func init() {
-	Init()
-}
-
-func Init() {
+func Init(path string) {
 	if os.Getenv("SKIP_ENV_FILE") != "true" {
 		// Load environment variables from .env file
-		var (
-			_, b, _, _ = runtime.Caller(0)
-			basepath   = filepath.Join(b, "..", "..", "..")
-		)
-		err := godotenv.Load(filepath.Join(basepath, ".env"))
+		err := godotenv.Load(path)
 		if err != nil {
 			log.Fatalf("failed to load env file, error: %v", err.Error())
 		}
-
 	}
 }
